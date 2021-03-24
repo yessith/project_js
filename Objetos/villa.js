2f5704761e6571c9fbb21420a4720d84b8f26100
Yessith Estiven Herrera Sanjuan <yessith.hs@gmail.com>
2021-03-24 14:54:15 -0500

    Subiendo ejemplo de practica objetos en JS

--- Objetos/villa.js
+++ Objetos/villa.js
@@ -1,18 +1,67 @@
 var lienzo = document.getElementById("lienzo");
-var ctx = lienzo.getContext("2d");
-var mapa = "img/tile.png";
-var cerso = "img/cerdo.png";
-var pollo = "img/pollo.png";
-var vaca = "img/vaca.png";
+var papel = lienzo.getContext("2d");
+var cantidad = random(0, 20);
+console.log(cantidad);
 
-var imagen = new Image();
-imagen.src = mapa;
+var mapa = {
+  ruta: "img/tile.png",
+  cargaOk: false,
+};
 
-var min = 10;
-var max = 20;
-var aleatorio;
+function Animal(ruta, cargaOk) {
+  this.ruta = ruta;
+  this.cargaOk = cargaOk;
+}
 
-function random() {
+mapa.imagen = new Image();
+mapa.imagen.src = mapa.ruta;
+mapa.imagen.addEventListener("load", cargarMapa);
+
+var cerdo = new Animal("img/cerdo.png", false);
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.ruta;
+cerdo.imagen.addEventListener("load", cargarCerdo);
+
+var vaca = new Animal("img/vaca.png", false);
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.ruta;
+vaca.imagen.addEventListener("load", cargarVaca);
+
+function cargarMapa() {
+  mapa.cargaOk = true;
+  dibujar();
+}
+function cargarCerdo() {
+  cerdo.cargaOk = true;
+  dibujar();
+}
+function cargarVaca() {
+  vaca.cargaOk = true;
+  dibujar();
+}
+
+function dibujar() {
+  if (mapa.cargaOk) {
+    papel.drawImage(mapa.imagen, 0, 0);
+  }
+  if (cerdo.cargaOk) {
+    for (i = 0; i < cantidad; i++) {
+      var position_x = random(0, 420);
+      var position_y = random(0, 420);
+      papel.drawImage(cerdo.imagen, position_x, position_y);
+    }
+  }
+  if (vaca.cargaOk) {
+    for (i = 0; i < cantidad; i++) {
+      var position_x = random(0, 420);
+      var position_y = random(0, 420);
+      papel.drawImage(vaca.imagen, position_x, position_y);
+    }
+  }
+}
+
+function random(min, max) {
+  var aleatorio;
   aleatorio = Math.floor(Math.random() * (max - min + 1) + min);
   return aleatorio;
 }
